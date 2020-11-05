029f76f7e603a3531f902d4e4e6b142857259f41
leonel-leyva <72991534+leonel-leyva@users.noreply.github.com>
2020-11-04 21:13:03 -0700

    Add files via upload

---
+++ trabajo 4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main() {
+	std::cout<<"ingrese un e ntero:";
+	int x{};
+	std::cin>>x;
+	if(x==0)
+	std::cout<<"el valor es cero\n";
+	else
+	std::cout<<"el valor no es cero:\n";
+	return 0;
+}
