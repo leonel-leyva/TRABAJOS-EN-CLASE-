e44759cae495fc0eda067955410e0209429f4439
leonel-leyva <72991534+leonel-leyva@users.noreply.github.com>
2020-11-10 21:04:22 -0700

    clases 10-11-2020

---
+++ trabajo2.cpp
@@ -0,0 +1,15 @@
+#include<bitset>//
+#include <iostream>
+
+
+
+int main() {
+	
+	std::bitset<8> bin1{0b1100'0101};
+	std::bistet<8> bin2{oxc5};
+	
+	std::cout << bin1<< ' ' << bin2 <<'\n';
+	std::cout << std::bitset<4>{0b1010}<<'\n';
+	return 0;
+	
+}
